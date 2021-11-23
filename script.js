07fe210a37e2d1f8848084f4827ba5418e893023
Swtg1337 <31216735+swtg1337@users.noreply.github.com>
2021-11-23 19:07:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-function registration() {
+function displayError(message) {
+	document.getElementById('error').innerHTML = `<b>Ошибка!</br>${message}</b>`;
+    document.getElementById('error').style.display = "block";
+    document.getElementById('error').style.color = "red";
+}   
+
+function success() {
+    document.getElementById('email').value = "";
+    document.getElementById('password').value = "";
+    document.getElementById('confirmpassword').value = "";
+    document.getElementById('error').style.color = "green";
+    document.getElementById('error').style.display = "block";
+    document.getElementById('error').innerHTML = '<b>Регистрация прошла успешно</b>';
+
+}
+
+function registration(){
     let mail = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     let confirmpassword = document.getElementById('confirmpassword').value;
-
-    if (mail == "" || password == "" || confirmpassword == ""){
-        alert("Не все поля заполнены")
-    } else {
-        if (mail.match(/.+@.+\..+/i) != mail) {
-            alert("Введен некорректный e-mail");
-        } else {
-            if (password.match(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/g) != password) {
-                alert('Пароль должен состоять не менее чем из 8 символов, а также не менее 1 символа верхнего и нижнего регистра и не менее 1 цифры');
-            } else {
-                if (password != confirmpassword) {
-                    alert('Пароли не совпадают');
-                } else {
-                    document.getElementById('email').value = "";
-                    document.getElementById('password').value = "";
-                    document.getElementById('confirmpassword').value = "";
-                    alert("Регистрация прошла успешно");
-                }
-            }
-        }
+    
+    if (!mail || !password || !confirmpassword ){
+        displayError ('Не все поля заполнены');
+        
+        return;
     }
-}
+
+    mail = mail.match(/^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/);
+
+    if (!mail) {
+        displayError('Введен некорректный e-mail!');
+    
+    return;
+    }
+
+    password = password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/g);
+
+    if (!password) {
+        displayError('Пароль должен состоять минимум из 8 символов, а также включать миниммум 1 символ верхнего регистра и 1 цифру');
+
+        return;
+    }
+
+    if (password != confirmpassword) {
+        displayError('Пароли не совпадают');
+
+        return;
+    }
+    
+    success();
+} 
